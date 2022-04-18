2ed07dd4ef0106c7ee41c42e4021b2f0f0df5a7e
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-04-18 22:31:34 +0900

    残りのCRUDを一気に作る

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Cat < ApplicationRecord
+  validates :name, presence: true
+
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
